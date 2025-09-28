b60e19546a728e90ad394e229e5d96a5067e1e52
Nithin Sankar <bnithinsankar@gmail.com>
2025-09-28 01:58:53 -0500

    Update Conditionals.class with new binary changes

---
+++ ArraysClass.java
@@ -0,0 +1,14 @@
+public class ArraysClass {
+
+    public static void main(String[] args) {
+
+       
+
+        int[] array = {13, 25, 321, 44, 56};
+
+        for (int i = 0; i < 6; i++) {
+            System.out.println(array[i]);
+        }
+    }
+    
+}
